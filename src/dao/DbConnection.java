e96cb530f9f5f11a593b11ed654471669563a554
bilal belhaj <bilalbelhadj2025@gmail.com>
2025-12-11 00:16:06 +0100

    Create Connection class to connect with Mysql database server

--- src/dao/DbConnection.java
+++ src/dao/DbConnection.java
@@ -1,7 +1,19 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author $(bilal belhaj)
  **/
 public class DbConnection {
+    private static final String url = "jdbc:mysql://localhost:3306";
+    private static final String user = "root";
+    private static final String pass = "bilal2025";
+    public static Connection conn;
+    public static Connection getConnection() throws SQLException {
+        conn = DriverManager.getConnection(url, user, pass);
+        return conn;
+    }
 }
