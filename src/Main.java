1c4bf2d34813ec05c7544bbf419704f13d0077c1
bilal belhaj <bilalbelhadj2025@gmail.com>
2025-12-11 00:45:56 +0100

    Improve the medicine class design by using Record to hold the medicine data

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import view.Login;
+
 /**
  * @author $(bilal belhaj)
  **/
 public class Main {
     public static void main(String[] args) {
         System.out.println("The journey starts here");
+        new Login();
     }
 }
---
+++ src/view/Login.java
@@ -0,0 +1,20 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author $(bilal belhaj)
+ **/
+public class Login extends JFrame {
+
+    public Login() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.getContentPane().setBackground(Color.BLACK);
+
+        this.setLayout(null);
+        this.setSize(1000, 700);
+        this.setVisible(true);
+    }
+}
