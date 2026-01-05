d75c2668ce84f463c813ef988c9456d5cb0b56e4
bilalbelhaj1 <bilalbelhadj2025@gmail.com>
2026-01-05 01:11:26 +0100

    Create Purchase Model

---
+++ src/model/Purchase.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+/**
+ * @author $(bilal belhaj)
+ **/
+public class Purchase {
+    private int id;
+    private final int supplierId;
+    private final LocalDate date;
+    private final BigDecimal total;
+
+    public Purchase(int id, int supplierId, LocalDate date, BigDecimal total) {
+        this.id = id;
+        this.supplierId = supplierId;
+        this.date = date;
+        this.total = total;
+    }
+
+    public Purchase(int supplierId, LocalDate date, BigDecimal total) {
+        this.supplierId = supplierId;
+        this.date = date;
+        this.total = total;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+    public int getSupplierId() {
+        return this.supplierId;
+    }
+    public LocalDate getDate() {
+        return this.date;
+    }
+
+    public BigDecimal getTotal() {
+        return this.total;
+    }
+}
